8b65e79c7875d9a051cc3e3e4f896bc4545ffdbb
dlstjr9068 <dlstjr9068@gmail.com>
2021-05-18 18:14:42 +0900

    스크린샷 경로 수정

--- frontend/src/utils/request.ts
+++ frontend/src/utils/request.ts
@@ -42,11 +42,11 @@ service.interceptors.response.use(
       })
       if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
         MessageBox.confirm(
-          '你已被登出，可以取消继续留在该页面，或者重新登录',
-          '确定登出',
+          '인증 만료되었습니다. 재인증 하시겠습니까?',
+          '인증',
           {
-            confirmButtonText: '重新登录',
-            cancelButtonText: '取消',
+            confirmButtonText: '재인증 하기',
+            cancelButtonText: '취소',
             type: 'warning'
           }
         ).then(() => {
